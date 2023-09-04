199f6f0cd18dc2d2e5169cd49dfe1c00e197b36e
marcelloCoder <deoliveiral767@gmail.com>
2023-09-04 15:26:16 -0300

    Inicio

---
+++ src/application/App.java
@@ -0,0 +1,16 @@
+package application;
+
+import javax.swing.JOptionPane;
+
+public class App {
+	
+	public Double valor;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		valor = JOptionPane.showConfirmDialog(null, args);
+
+	}
+
+}
